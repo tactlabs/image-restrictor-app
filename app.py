e35b351f425f8c344c3ad3e96eec0f790cd1836a
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2021-07-29 22:05:34 +0530

    img size restriction added and new html file created

--- app.py
+++ app.py
@@ -1,47 +1,45 @@
-from flask import Flask,render_template,request,flash,redirect,url_for
-from werkzeug.utils import secure_filename
-import urllib.request
+from flask import Flask,render_template,request,redirect,flash
 import os
+from werkzeug.utils import secure_filename
+from PIL import Image
+
 
 app = Flask(__name__)
-PORT = 3000
-UPLOAD_FOLDER = 'static/uploads/'
+PORT = 3001
 
+UPLOAD_FOLDER = 'static/uploads/'
 app.secret_key = "34c75cac10fb587135e2b27ef5765007"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] =  16 * 1024 * 1024
-
-ALLOWED_EXTENSIONS = set(['png' , 'jpg' ,'jpeg'])
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route('/')
-def home():
-    return render_template("index.html")
 
 @app.route('/',methods=["GET","POST"])
 def upload_image():
-    if 'file' not in request.files:
-        flash("No file part")
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash("No image selected for uploading!")
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-        flash("Image successfully uploaded and displayed below")
-        return render_template("index.html",filename = filename)
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash("No file part!")
+            return redirect(request.url)
+        file = request.files['file']
+
+        if file.filename == '':
+            flash("No image selected for uploading!")
+            return redirect(request.url)
+
+        if file.filename != '':
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'img.jpg'))
+            image =Image.open("static/uploads/img.jpg")  
+            width1, height1 = image.size
+            if width1 == 120 and  height1 == 120:
+                # return "File uploaded successfully!"
+                return render_template("result.html",width1 = width1 , height1 =height1)
+            elif width1 == 140 and height1 == 120:
+                return render_template("result.html",width1 = width1 , height1 =height1)
+
+            else:
+                return "<h3>Error...!! image must be exactly in this size only (120 X 120) or (140 X 120).</h3>"
     else:
-        flash("Images with size (120 x 120) or (140 x 120) are only allowed! ")
-        return redirect(request.url)
+        return render_template("index.html")        
 
-@app.route('/display/<filename>')
-def display_image(filename):
-    return redirect(url_for('static',filename='uploads/' + filename),code=301)
-
+     
+            
 if __name__ == '__main__':
     app.run(debug=True,port=PORT)
